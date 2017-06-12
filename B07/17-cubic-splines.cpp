0edeba84f9ee0df2bef7d3ea0b4aa8a874714c08
Martin Pittermann <martinp2250@gmail.com>
2017-06-12 16:33:46 +0200

    07 - improved cubic splines

--- B07/17-cubic-splines.cpp
+++ B07/17-cubic-splines.cpp
@@ -2,15 +2,15 @@
 #include <string.h>
 #include <fstream>
 
-const int maxl = 20;
+int maxl = 5;
 int length = 0;
 
-double X[maxl], Y[maxl];
+double *X, *Y;
 
-double mu[maxl], d[maxl], M[maxl];
+double *mu, *d, *M;
 
 //renamed gamma as there is already a function with that name
-double beta[maxl], gamma_a[maxl], delta[maxl];
+double *beta, *gamma_a, *delta;
 
 double lambda(int j)
 {
@@ -43,6 +43,7 @@ double d_init(int j)
 	return 6. * (a - b) / (X[j + 1] - X[j - 1]);
 }
 
+//interpolated values
 double S(double x)
 {
 	int j = 0;
@@ -66,16 +67,32 @@ double S(double x)
 
 int main(int argc, char **argv)
 {
+	X = (double*)malloc(maxl * sizeof(double));
+	Y = (double*)malloc(maxl * sizeof(double));
+
 	std::ifstream input("../B06/a16-interpol.dat");
 
-	while((length < maxl) && (input >> X[length] >> Y[length]))
+	while(input >> X[length] >> Y[length])
 	{
-		length++;
+		if(++length == maxl)
+		{
+			maxl += 5;
+			X = (double*)realloc(X, maxl * sizeof(double));
+			Y = (double*)realloc(Y, maxl * sizeof(double));
+			std::cout << "reallocated X, Y. new size: " << maxl << std::endl;
+		}
 	}
 
 	input.close();
 
 	std::cout << "loaded " << length << " data points\n";
+
+	mu = (double*)malloc(maxl * sizeof(double));
+	d = (double*)malloc(maxl * sizeof(double));
+	M = (double*)malloc(maxl * sizeof(double));
+	beta = (double*)malloc(maxl * sizeof(double));
+	gamma_a = (double*)malloc(maxl * sizeof(double));
+	delta = (double*)malloc(maxl * sizeof(double));
 
 	//populate arrays with initial values
 	for(int i = 0; i < length; i++)
@@ -120,4 +137,8 @@ int main(int argc, char **argv)
 	}
 
 	output.close();
+
+	std::cout << "saved " << points << " interpolated points to output file" << std::endl;
+
+	//no need to free memory, the kernel does that
 }
--- B07/plot.py
+++ B07/plot.py
@@ -3,8 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 (X, Y) = np.loadtxt('interpolated.dat', unpack=True)
+(X_old, Y_old) = np.loadtxt('../B06/interpolated.dat', unpack=True)
 (Xo, Yo) = np.loadtxt('../B06/a16-interpol.dat', unpack=True)
 
-plt.plot(X, Y)
-plt.plot(Xo, Yo, 'o')
+plt.plot(X_old, Y_old, 'g', label='polynomial')
+plt.plot(X, Y, 'b', label='cubic splines')
+plt.plot(Xo, Yo, 'or')
+plt.legend()
 plt.show()
